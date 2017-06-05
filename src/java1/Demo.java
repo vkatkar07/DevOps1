93dbf4ed8a80d30c5f7ae795cceae86e86cbb1e8
vkatkar07 <varsha.katkar@capgemini.com>
2017-06-05 18:21:58 +0530

    1st proj

---
+++ src/java1/Demo.java
@@ -0,0 +1,17 @@
+package java1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10;
+		int b=20;
+		
+		int c=a+b;
+		
+		System.out.println("Addition is = "+c);
+	}
+
+}
